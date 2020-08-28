1224fa1ca8824257650f5b94eae26542f7a2e8d7
Brittany Allesandro <brittany.allesandro@gmail.com>
2020-08-28 11:57:36 -0500

    Added character controller and in memory db.

---
+++ Models/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace dndbeyond.Models
+{
+    public class Character
+    {
+        public Character()
+        {
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/CharacterContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace dndbeyond.Models
+{
+    public class CharactersContext : DbContext
+    {
+        public CharactersContext()
+        {
+        }
+
+        public CharactersContext(DbContextOptions<CharactersContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
